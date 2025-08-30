594db534aa5e148d822e002022ab172f12b82381
Ryan Bucinell <rbucinell.dev@gmail.com>
2025-08-29 21:50:27 -0400

    vercel support and rearage header

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import node from '@astrojs/node';
+import vercelServerless from '@astrojs/vercel/serverless';
 
 export default defineConfig({
     output: 'server',
+    adapter: vercelServerless(),
     vite: {
       plugins: [ tailwindcss() ],
       server: {
@@ -13,8 +15,5 @@ export default defineConfig({
         },
       },
     },
-    adapter: node({
-      mode: 'standalone'
-    }),
     integrations: [icon()]
 });
