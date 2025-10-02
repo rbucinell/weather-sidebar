5716150452a780caa94441b8a2970c7e9f48910f
Ryan Bucinell <rbucinell.dev@gmail.com>
2025-10-02 07:19:56 -0400

    removed vercel processing + vercel update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import node from '@astrojs/node';
-import vercelServerless from '@astrojs/vercel/serverless';
+import vercelServerless from '@astrojs/vercel';
 
 export default defineConfig({
     //output: 'static',
